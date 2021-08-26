ce69b3d802edcd7c5094c2dc12bc3991901d6072
Daniel Artine <dan.artine@gmail.com>
2021-08-25 22:27:31 -0300

    Update Mensagem.cs

--- UsuariosApi/Models/Mensagem.cs
+++ UsuariosApi/Models/Mensagem.cs
@@ -1,4 +1,4 @@
-﻿using MimeKit;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +8,17 @@ namespace UsuariosApi.Models
 {
     public class Mensagem
     {
-        public List<MailboxAddress> To { get; set; }
-        public string Subject { get; set; }
-        public string Content { get; set; }
-        public Mensagem(IEnumerable<string> to, string subject, int usuarioId, string content)
+        public List<MailboxAddress> Destinatario { get; set; }
+        public string Assunto { get; set; }
+        public string Conteudo { get; set; }
+
+        public Mensagem(IEnumerable<string> destinatario, string assunto,
+            int usuarioId, string codigo)
         {
-            To = new List<MailboxAddress>();
-            To.AddRange(to.Select(x => new MailboxAddress(x)));
-            Subject = subject;
-            Content = $"http://localhost:6000/ativa?UsuarioId={usuarioId}&CodigoDeAtivacao={content}";
+            Destinatario = new List<MailboxAddress>();
+            Destinatario.AddRange(destinatario.Select(d => new MailboxAddress(d)));
+            Assunto = assunto;
+            Conteudo = $"http://localhost:6000/ativa?UsuarioId={usuarioId}&CodigoDeAtivacao={codigo}";
         }
     }
 }
