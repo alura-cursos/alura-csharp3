bf6384d35432da16b8cf9831d2fa37fbf0ea1c9f
Daniel Artine <dan.artine@gmail.com>
2021-08-25 22:25:56 -0300

    Update CadastroController.cs

--- UsuariosApi/Controllers/CadastroController.cs
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -1,17 +1,17 @@
-﻿using FluentResults;
+using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UsuariosApi.Data.Dtos.Usuario;
+using UsuariosApi.Data.Dtos;
 using UsuariosApi.Data.Requests;
 using UsuariosApi.Services;
 
 namespace UsuariosApi.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class CadastroController : ControllerBase
     {
         private CadastroService _cadastroService;
@@ -28,6 +28,7 @@ namespace UsuariosApi.Controllers
             if (resultado.IsFailed) return StatusCode(500);
             return Ok(resultado.Successes);
         }
+
         [HttpGet("/ativa")]
         public IActionResult AtivaContaUsuario([FromQuery] AtivaContaRequest request)
         {
