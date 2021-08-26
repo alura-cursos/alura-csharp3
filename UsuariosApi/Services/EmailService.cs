5528080ba32a1f3bb260e8a3b6ceef4272890993
Daniel Artine <dan.artine@gmail.com>
2021-08-25 22:26:54 -0300

    Update EmailService.cs

--- UsuariosApi/Services/EmailService.cs
+++ UsuariosApi/Services/EmailService.cs
@@ -1,8 +1,10 @@
-﻿using MailKit.Net.Smtp;
+using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using MimeKit;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using UsuariosApi.Models;
 
 namespace UsuariosApi.Services
@@ -10,44 +12,34 @@ namespace UsuariosApi.Services
     public class EmailService
     {
 
-        private readonly IConfiguration _configuration;
+        private IConfiguration _configuration;
 
         public EmailService(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        public void EnviarEmail(string[] to, string subject, int usuarioId, string content)
+        public void EnviarEmail(string[] destinatario, string assunto,
+            int usuarioId, string code)
         {
-            var message = new Mensagem(to, subject, usuarioId, content);
-            var emailMessage = CriaCorpoDoEmail(message);
-            Enviar(emailMessage);
+            Mensagem mensagem = new Mensagem(destinatario,
+                assunto, usuarioId, code);
+            var mensagemDeEmail = CriaCorpoDoEmail(mensagem);
+            Enviar(mensagemDeEmail);
         }
 
-        private MimeMessage CriaCorpoDoEmail(Mensagem message)
-        {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(_configuration.GetValue<string>("EmailSettings:From")));
-            emailMessage.To.AddRange(message.To);
-            emailMessage.Subject = message.Subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
-            return emailMessage;
-        }
-
-        private void Enviar(MimeMessage mailMessage)
+        private void Enviar(MimeMessage mensagemDeEmail)
         {
             using (var client = new SmtpClient())
             {
                 try
                 {
-                    Console.WriteLine(_configuration.GetValue<string>("EmailSettings:SmtpServer"));
-                    Console.WriteLine(_configuration.GetValue<string>("EmailSettings:From"));
-                    Console.WriteLine(_configuration.GetValue<string>("EmailSettings:Password"));
-                    Console.WriteLine(_configuration.GetValue<int>("EmailSettings:Port"));
-                    client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"), _configuration.GetValue<int>("EmailSettings:Port"), true);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    client.Authenticate(_configuration.GetValue<string>("EmailSettings:From"), _configuration.GetValue<string>("EmailSettings:Password"));
-                    client.Send(mailMessage);
+                    client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"),
+                        _configuration.GetValue<int>("EmailSettings:Port"), true);
+                    client.AuthenticationMechanisms.Remove("XOUATH2");
+                    client.Authenticate(_configuration.GetValue<string>("EmailSettings:From"),
+                        _configuration.GetValue<string>("EmailSettings:Password"));
+                    client.Send(mensagemDeEmail);
                 }
                 catch
                 {
@@ -59,6 +51,21 @@ namespace UsuariosApi.Services
                     client.Dispose();
                 }
             }
+        }
+
+        private MimeMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress(
+                _configuration.GetValue<string>("EmailSettings:From")));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatario);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+
+            return mensagemDeEmail;
         }
     }
 }
